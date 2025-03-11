21e55b72d69b55d3f569e6544f51db7b7744bfc5
Steve Raiff <sraiff@natera.com>
2025-03-10 18:15:32 -0700

    finished e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -8,52 +8,53 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-import * as crypto from 'crypto';
-
-// Have to do this because of incomptibilities betweeen nest and express
 const cookieSession = require('cookie-session');
 
-// Easiest way to get the database name to change based on the environment
-// database: (process.env.NODE_ENV === 'test') ? 'test.sqlite' : 'db.sqlite',
-
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: `.env.${process.env.NODE_ENV}`,
-  }), UsersModule, ReportsModule, 
-  TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory: (config: ConfigService) => {
-      return {
-        type: 'sqlite',
-        database: config.get<string>('DB_NAME'),
-        synchronize: true,
-        entities: [User, Report],
-      };
-    }
-  })],
-  //TypeOrmModule.forRoot({
-  //  type: 'sqlite',
-   // database: ConfigService.get('DATABASE_NAME'),
-    //entities: [User, Report],
-    //synchronize: true,
-  //})],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
         whitelist: true,
-      })
-    }
+      }),
+    },
   ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(
-      cookieSession({
-        keys: ['asdfasdklfk']
-      })
-    ).forRoutes('*');
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asdfasfd'],
+        }),
+      )
+      .forRoutes('*');
   }
 }
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -21,7 +21,7 @@ describe('Authentication System', () => {
 
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({email: email, password: 'asdf'})
+      .send({ email: email, password: 'asdf' })
       .expect(201)
       .then((res) => {
         // console.log(res.body);
@@ -29,5 +29,24 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       })
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdfklajsdfjklf@lakjsdlj.com';
+
+    // superagent doesn't handle cookies by default
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie') || [''];
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
